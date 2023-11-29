df4a351870fb9ccf6f9b4ac1fd29d195d3993bb7
nahuel11500 <nahuel.canavy@imt-atlantique.net>
2023-11-29 10:34:51 +0100

    Added joueur services

--- entry_point/user.py
+++ entry_point/user.py
@@ -21,10 +21,11 @@ movie_service_url = os.getenv('MOVIE_SERVICE_URL', 'http://127.0.0.1:3200')
 #URL de secours si la variable d'environnement n'est pas définie
 if not booking_service_url:
     movie_service_url = 'http://127.0.0.1:3201'
+
+
 @app.route("/", methods=['GET'])
 def home():
     return "<h1 style='color:blue'>Welcome to the User service!</h1>"
-
 
 
 if __name__ == "__main__":
