adb1b5a3c4f8fb6328cc3d2b48cc00336316adcb
nahuel11500 <nahuel.canavy@imt-atlantique.net>
2023-11-30 10:40:45 +0100

    Updated user information

--- user_information/user_information.py
+++ user_information/user_information.py
@@ -7,14 +7,23 @@ from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
-PORT = 3200
+PORT = 3206
 HOST = '0.0.0.0'
+
+
 absolute_path = os.path.dirname(__file__)
 relative_path = "databases/user_information.json"
 full_path = os.path.join(absolute_path, relative_path)
-with open(full_path, "r") as jsf:
-   user_information = json.load(jsf)["user_informations"]
 
+# Fonction pour charger les informations utilisateur
+def load_user_information():
+    with open(full_path, "r") as jsf:
+        return json.load(jsf)["user_informations"]
+
+# Fonction pour sauvegarder les informations utilisateur
+def save_user_information(user_information):
+    with open(full_path, "w") as jsf:
+        json.dump({"user_informations": user_information}, jsf)
 # root message
 #this is the welcome message
 @app.route("/", methods=['GET'])
@@ -22,21 +31,34 @@ def home():
     return make_response("<h1 style='color:blue'>Welcome to the User Information service!</h1>",200)
 
 
-@app.route("/creation_player",methods=['GET'])
+@app.route("/creation_player",methods=['POST'])
 def create_user():
     """This function get the username and the password in the body and create a new player"""
-    password = request.params()['password']
-    username = request.params()['username']
+    # Charger les informations utilisateur
+    user_information = load_user_information()  
 
-    ## Verify that the username doesn't already exist
-    ### Create the new player
-    new_player = {
-        "password" : password,
-        "username" : username,
-        "role" : "player"
-    }
-    user_information.append(new_player)
-    return make_response("Player added successfully", 200)
+    # Get data from request
+    data = request.get_json()  # Assuming the data is sent in JSON format
+    if data:
+        username = data.get('username')
+        password = data.get('password')
+
+        # Vérifier si le nom d'utilisateur existe déjà
+        for user in user_information:
+            if username == user["username"]:
+                return make_response("Username already exists", 400)
+
+        # Sinon, créer le nouveau joueur
+        new_player = {
+            "username": username,
+            "password": password,
+            "role": "player"
+        }
+        user_information.append(new_player)
+        save_user_information(user_information)  # Sauvegarder les nouvelles informations
+        return make_response("Player added successfully", 200)
+    else:
+        return make_response("Invalid data provided", 400)
 
 if __name__ == "__main__":
     #p = sys.argv[1]
