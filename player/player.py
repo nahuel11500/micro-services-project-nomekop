6e305a60c20fca572670e74d2e2fd463738579c6
nahuel11500 <nahuel.canavy@imt-atlantique.net>
2023-11-30 09:50:45 +0100

    Typo

--- player/player.py
+++ player/player.py
@@ -17,24 +17,25 @@ times_service_url = os.getenv('TIMES_SERVICE_URL', 'http://127.0.0.1:3202')
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+   return "<h1 style='color:blue'>Welcome to the Player service!</h1>"
 
+## Return the list of players
 @app.route("/players", methods=['GET'])
 def get_players():
    res = make_response(jsonify(players), 200)
    return res
 
 
-## Return the list of players
+## Return the information about one player
 @app.route("/player/<player_name>", methods=['GET'])
 def get_player(player_name):
    for player in players:
-      if player["username"] = player_name:
+      if player["username"] == player_name:
          return make_response(jsonify(player), 200)
    return(make_response("Player not found", 400))
 
 
-## Return the information of a player based on his name
+## Add a player
 @app.route("/player/<player_name>", methods=['POST'])
 def create_player(player_name):
    infos = {
@@ -59,7 +60,7 @@ def create_match(player_name,player_request):
 @app.route("/buy/<player_name>/<nomekop>", methods=['PUT'])
 def buy_nomekop(player_name, nomekop):
    for player in players:
-      if player["username"] = player_name:
+      if player["username"] == player_name:
          price = requests.get(f'http://localhost:{PORT_STORE}/getNomekopPrice/{nomekop}')
          if player["Crédit"] >= price:
                player["Crédit"] -= price
